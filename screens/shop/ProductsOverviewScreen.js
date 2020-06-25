c9a48904f51683a2c3532cfeffcd8fca45bce536
Stephan <sgriesel@gmail.com>
2020-06-25 07:10:53 +0200

    fix typo

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 
 const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
-    return <FlatList data={products} keyExtractor={item = item.id} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
+    return <FlatList data={products} keyExtractor={item => item.id} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
 };
 
 ProductsOverviewScreen.navigationOptions = {
