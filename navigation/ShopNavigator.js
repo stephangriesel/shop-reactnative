c5dd629faf145c1805ffe203b5420d12a7bde139
Stephan <sgriesel@gmail.com>
2020-06-25 07:35:13 +0200

    test stack navigator - wip

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,4 +1,20 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+// import { createStackNavigator } from '@react-navigation/stack';
+
+// https://reactnavigation.org/docs/stack-navigator/
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Notifications" component={Notifications} />
+            <Stack.Screen name="Profile" component={Profile} />
+            <Stack.Screen name="Settings" component={Settings} />
+        </Stack.Navigator>
+    );
+}
 import { Platform } from 'react-native';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,7 +1,7 @@
 import PRODUCTS from '../../data/dummy-data';
 
 const initialState = {
-    availableProducts = PRODUCTS,
+    availableProducts: PRODUCTS,
     userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1')
 };
 
