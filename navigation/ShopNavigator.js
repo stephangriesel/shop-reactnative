c55518c101a5bf271835e0cf8ee31567b2114beb
Stephan <sgriesel@gmail.com>
2020-06-26 07:02:43 +0200

    still trying to fix navigator - wip

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,35 +1,25 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-// import { createStackNavigator } from '@react-navigation/stack';
+// import { NavigationContainer } from '@react-navigation/native';
 
 // https://reactnavigation.org/docs/stack-navigator/
 
-const Stack = createStackNavigator();
-
-function MyStack() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Notifications" component={Notifications} />
-            <Stack.Screen name="Profile" component={Profile} />
-            <Stack.Screen name="Settings" component={Settings} />
-        </Stack.Navigator>
-    );
-}
 import { Platform } from 'react-native';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
-
 import Colors from '../constants/Colors';
 
-const ProductsNavigator = createStackNavigator({
-    ProductsOverview: ProductsOverviewScreen
-}, {
-    defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+const ProductsNavigator = createStackNavigator(
+    {
+        ProductsOverview: ProductsOverviewScreen
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+            },
+            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+        }
     }
-});
+);
 
 export default createAppContainer(ProductsNavigator);
--- store/reducers/products.js
+++ store/reducers/products.js
@@ -7,4 +7,4 @@ const initialState = {
 
 export default (state = initialState, action) => {
     return state;
-};
+}; 
