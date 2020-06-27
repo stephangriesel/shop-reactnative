bba7e1bc637a1fa29aa9155f141d31e0b36f9f5b
Stephan <sgriesel@gmail.com>
2020-06-27 08:04:16 +0200

    navigator working - styles not applied

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,25 +1,14 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-// import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
 
 // https://reactnavigation.org/docs/stack-navigator/
-
-import { Platform } from 'react-native';
-
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
-import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator(
     {
         ProductsOverview: ProductsOverviewScreen
-    },
-    {
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-            },
-            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-        }
     }
 );
+
 
 export default createAppContainer(ProductsNavigator);
