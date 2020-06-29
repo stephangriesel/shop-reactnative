2898507146628bde110e83fbe2171c46ee08c3c2
Stephan <sgriesel@gmail.com>
2020-06-29 06:54:53 +0200

    add detail identifier

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -4,12 +4,14 @@ import { createAppContainer } from "react-navigation";
 import { Platform } from 'react-native';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 
 import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator(
     {
-        ProductsOverview: ProductsOverviewScreen
+        ProductsOverview: ProductsOverviewScreen,
+        ProductDetail: ProductDetailScreen
     },
     {
         defaultNavigationOptions: {
