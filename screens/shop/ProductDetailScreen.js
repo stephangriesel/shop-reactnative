0eb8d54711d2d5d9203da021c8dc4b1791389ead
Stephan <sgriesel@gmail.com>
2020-06-29 06:51:48 +0200

    render details screen

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Image, Button, Stylesheet, ScrollView } from 'react-native';
+
+const ProductDetailsScreen = props => {
+    return (
+        <View>
+            <Text>Details</Text>
+        </View>
+    );
+}
+
+const styles = Stylesheet.create({});
+
+export default ProductDetailsScreen;
