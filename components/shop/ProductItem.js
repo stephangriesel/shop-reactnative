587d48d984e1417c7c56d41a33cb8c4cf9a14eee
Stephan <sgriesel@gmail.com>
2020-06-29 07:21:38 +0200

    link product image

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, Button } from 'react-native';
+import { View, Text, Image, StyleSheet, Button, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
 import Colors from '../../constants/Colors';
 
 const ProductItem = props => {
-    return <View style={styles.product}>
-        <View style={styles.imageContainer}>
-            <Image style={styles.image} source={{ uri: props.image }} />
-        </View>
-        <View style={styles.details}>
-            <Text style={styles.title}>{props.title}</Text>
-            <Text style={styles.price}>{props.price.toFixed(2)}</Text>
-        </View>
-        <View style={styles.actions}>
-            <Button color={Colors.primary} title="Details" onPress={props.onViewDetail}></Button>
-            <Button color={Colors.primary} title="Add To Cart" onPress={props.onAddToCart}></Button>
-        </View>
-    </View>
+    return (
+        <TouchableOpacity onPress={props.onViewDetail}>
+            <View style={styles.product}>
+                <View style={styles.imageContainer}>
+                    <Image style={styles.image} source={{ uri: props.image }} />
+                </View>
+                <View style={styles.details}>
+                    <Text style={styles.title}>{props.title}</Text>
+                    <Text style={styles.price}>{props.price.toFixed(2)}</Text>
+                </View>
+                <View style={styles.actions}>
+                    <Button color={Colors.primary} title="Details" onPress={props.onViewDetail}></Button>
+                    <Button color={Colors.primary} title="Add To Cart" onPress={props.onAddToCart}></Button>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
 };
 
 const styles = StyleSheet.create({
