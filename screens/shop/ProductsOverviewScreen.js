86f3aceae0cce77b8f24ec423b6e9873d9576dda
Stephan <sgriesel@gmail.com>
2020-06-29 06:56:56 +0200

    add detail route

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -14,7 +14,9 @@ const ProductsOverviewScreen = props => {
                 image={itemData.item.imageUrl}
                 title={itemData.item.title}
                 price={itemData.item.price}
-                onViewDetail={() => { }}
+                onViewDetail={() => {
+                    props.navigation.navigate({ routeName: 'ProductDetail' })
+                }}
                 onaAddToCart={() => { }}
             />} />
 };
